a7f4faed6094f018b1ce75150a12d7c699fc3c9a
Ruud van Falier <ruud@partechit.nl>
2019-04-08 14:02:59 +0200

    Implemented and tested all jump instructions

--- Code/Assembler/test.asm
+++ Code/Assembler/test.asm
@@ -1,14 +1,34 @@
 :main
 
-	MVI A 0xFF
-	
-:again
-	MOV A AX
-	SHR A
-	
-	JNZ again
+	MVI AX 0xFE
+	MVI AY 0xF1
+	CMP
 
-:done
+	JGT equal
+	JLT notequal
+
+:resume
+	MVI AX 0x03
+	MVI AY 0x05
+	CMP
+
+	JLT equal2
+	JGT notequal2
+
+	HALT
+
+:equal
+	MVI A 0x11
+	JMP resume
+
+:notequal
+	MVI A 0x22
+	JMP resume
+
+:equal2
 	MVI B 0x11
-	
+	HALT
+
+:notequal2
+	MVI B 0x22
 	HALT
