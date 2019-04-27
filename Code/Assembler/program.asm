f7cbc064cdda1b0789012be6b714751cdc3b1e93
Ruud van Falier <ruud@partechit.nl>
2019-04-27 08:54:56 +0200

    Added CALLC instructions

--- Code/Assembler/program.asm
+++ Code/Assembler/program.asm
@@ -1,24 +1,17 @@
 :main
 
 	MVI AX 0x00
-	MVI AY 0x0A
-	SUB A
-
-	JMPC C carry
-	MVI B 0x02
-
-	MVI AX 0x02
 	MVI AY 0x01
-	SUB A
+	ADD CMP
 
-	JMPC C carry2
-	MVI A 0x02
+	CALLC C carry
+	MVI B 0x02
 
 	HALT
 
 :carry
-	MVI B 0x01
-	HALT
+	MVI A 0x01
+	RET
 
 :carry2
 	MVI A 0x01
